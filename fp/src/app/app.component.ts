e1e5adb0130bbc41b2f57fc2415430920ed9580a
MileenCitrusbug <102975727+MileenCitrusbug@users.noreply.github.com>
2022-06-23 18:34:08 +0530

    ang 2

--- fp/src/app/app.component.ts
+++ fp/src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { isPlatformBrowser } from '@angular/common';
 import { Component } from '@angular/core';
+import { reduce } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +8,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fp';
-  count() {
-    
-    return 'i';
-  }
+ipvalue=''  
+count=0  
+name=true
+color= false
+ip=''
+option=''
+fcolor='yellow'
+bgcolor='maroon'
+aler1(name:any){
+  alert(name);
+}
+eventshow(data:any){
+  console.log(data)
+}
+tsdata(value:string){
+this.ipvalue=value
+}
+counter(type:string){
+type==='+1' ? this.count++ :this.count--
+}
+pbinding(type:boolean){
+  type===true ? this.name=false : this.name=true 
+}
+
+ifcon(type:boolean){
+  type===true ?this.color=false: this.color=true
+
+}
+multiif(val:any){
+  this.ip=val
+
+}
+switchcase(val:any){
+  this.option=val
+}
+fcol(){
+this.fcolor=='yellow' ? this.fcolor= 'pink' : this.fcolor='yellow'
+}
+bgcol(){
+  this.bgcolor=='maroon' ? this.bgcolor= 'black' : this.bgcolor='maroon'
+}
+
+
 }
--- fp/src/app/app.module.ts
+++ fp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HomeModule} from './home/home.module' 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
@@ -12,7 +12,8 @@ import { RegistrationComponent } from './registration/registration.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
